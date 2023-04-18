74877972475b4ab973a954ff2d578462b05f770f
Saida Murtazali <saida.murtazaliyeva@yahoo.com>
2023-04-18 19:48:58 +0400

    Add validations for models

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,2 +1,5 @@
 class Package < ApplicationRecord
+  validates :package_name, presence: true
+  validates :package_description, presence: true
+  validates :package_price, presence: true, numericality: { greater_than: 0 }
 end
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,3 +1,13 @@
 class Payment < ApplicationRecord
   belongs_to :package
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :phone, presence: true, numericality: { only_integer: true }
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :country, presence: true
+  validates :payment_date, presence: true
+  validates :payment_amount, presence: true, numericality: { greater_than: 0 }
 end
