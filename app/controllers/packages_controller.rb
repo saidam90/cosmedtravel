56159da05f06d7a9ffe6ed1217c5238de69d1937
saida murtazali <saida.murtazaliyeva@yahoo.com>
2023-04-20 20:52:36 +0400

    Fix bug in package controller

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -4,11 +4,12 @@ class PackagesController < ApplicationController
   end
 
   def show
-    @package = Package.find(params[:package_id])
+    @package = Package.find(params[:id])
     @payment = Payment.new
   end
 
   private
+
   def package_params
     params.require(:package).permit(:package_name, :package_description, :package_price)
   end
