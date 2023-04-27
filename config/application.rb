aafb85aa51cd06d7d7fa934e0439e60cce71e7ed
saida murtazali <saida.murtazaliyeva@yahoo.com>
2023-04-27 20:45:49 +0400

    db test 8

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Cosmedtravel
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    config.paths["config/database"] = ["render.yaml"]
+    # config.paths["config/database"] = ["render.yaml"]
 
   end
 end
