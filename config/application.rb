53599c96acb98434958eae75a6053f93398f25ac
saida murtazali <saida.murtazaliyeva@yahoo.com>
2023-04-27 15:13:53 +0400

    Change config path in application.rb

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module Cosmedtravel
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.paths["config/database"] = ["render.yaml"]
+
   end
 end
