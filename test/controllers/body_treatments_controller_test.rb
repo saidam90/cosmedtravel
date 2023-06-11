c0bcae420cba715e5b213d48cd9520cbcf115fab
saidam90 <saida.murtazaliyeva@yahoo.com>
2023-06-11 18:42:07 +0400

    Create Hair treatments and Body treatments controllers

---
+++ test/controllers/body_treatments_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class BodyTreatmentsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/controllers/hair_treatments_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class HairTreatmentsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
