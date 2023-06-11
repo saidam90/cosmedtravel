6fc1139f8aa2322ad8c910f9a85f8d5fc106becc
saidam90 <saida.murtazaliyeva@yahoo.com>
2023-06-11 20:16:39 +0400

    Create seeds for Body treatments

--- db/seeds.rb
+++ db/seeds.rb
@@ -61,7 +61,7 @@ blepharoplasty = FaceTreatment.find_or_create_by(
 
 dermal_fillers = FaceTreatment.find_or_create_by(
   face_treatment_name: "Dermal Fillers",
-  face_treatment_description: "At Cosmed Travel, our team of cosmetic experts offers dermal fillers at highly affordable rates. Experience a transformative and refreshed look at our medical facilities, all while saving a significant amount compared to prices in Europe or US. Your desired aesthetic can be achieved without breaking the bank at Cosmed Travel."
+  face_treatment_description: "At Cosmed Travel, our team of cosmetic experts offers dermal fillers at highly affordable rates. Experience a transformative and refreshed look at our medical facilities, all while saving a significant amount compared to prices in Europe. Your desired aesthetic can be achieved without breaking the bank at Cosmed Travel."
 )
 
 anti_wrinkle_injections = FaceTreatment.find_or_create_by(
@@ -89,3 +89,47 @@ eyebrow_transplant = HairTreatment.find_or_create_by(
 )
 
 puts "hair treatments created"
+
+puts "creating body treatments"
+
+abdominoplasty = BodyTreatment.find_or_create_by(
+  body_treatment_name: "Abdominoplasty",
+  body_treatment_description: "Cosmed Travel presents abdominoplasty procedures conducted by world-class surgeons in our top-tier, 5-star hospital facilities, all at an affordable price. If you desire to eliminate excess skin and stubborn fat from your abdomen, we are here to assist you. Experience a smoother and more sculpted waistline, ultimately boosting your self-confidence. Trust us to help you achieve your aesthetic goals and enhance your overall well-being."
+)
+
+gastric_bypass = BodyTreatment.find_or_create_by(
+  body_treatment_name: "Gastric Bypass",
+  body_treatment_description: "Gastric bypass surgery is an effective solution for weight loss that can alleviate health concerns and significantly enhance quality of life. By choosing Cosmed Travel, you can save up to 50% on your procedure without any compromise on the quality of your medical team, healthcare, and overall well-being. Trust us to provide exceptional care and support throughout your journey to a healthier and happier life."
+)
+
+breast_lift = BodyTreatment.find_or_create_by(
+  body_treatment_name: "Breast Lift",
+  body_treatment_description: "Cosmed Travel's team of cosmetic and plastic experts specializes in breast lifts at prices that are 30% to 50% lower compared to Europe. Our surgeons possess extensive skills and experience, along with world-class accreditations and internationally recognized qualifications. You can trust our team to deliver exceptional results and provide the highest level of care throughout your breast lift journey. Experience the expertise of our surgeons and enjoy significant cost savings by choosing Cosmed Travel."
+)
+
+breast_implants = BodyTreatment.find_or_create_by(
+  body_treatment_name: "Breast Implants",
+  body_treatment_description: "If you desire breast implants to enhance the fullness of your breasts, Cosmed Travel's skilled breast surgeons are here to assist you. With their world-class expertise, extensive qualifications, and international accreditations, Cosmed Travel ensures the highest standards in breast implant procedures. Trust our team to deliver exceptional results and provide the best possible care throughout your breast enhancement journey. Experience the expertise and quality of Cosmed Travel for the breast implant procedure you desire."
+)
+
+breast_reduction = BodyTreatment.find_or_create_by(
+  body_treatment_name: "Breast Reduction",
+  body_treatment_description: "Cosmed Travel proudly provides breast reduction procedures at affordable prices, featuring world-class cosmetic surgeons and 5-star hospitals and facilities. Achieve the desired results you seek without compromising the quality of your healthcare. Our commitment to excellence ensures that you receive top-notch care and optimal outcomes throughout your breast reduction journey. Trust Cosmed Travel to prioritize your well-being and deliver exceptional results in a cost-effective manner."
+)
+
+brachioplasty = BodyTreatment.find_or_create_by(
+  body_treatment_name: "Brachioplasty",
+  body_treatment_description: "Cosmed Travel specializes in brachioplasty, commonly known as arm lift surgery, performed by world-class surgeons at significantly reduced prices compared to Europe. Achieve the desired results you seek without compromising on the quality of your healthcare. Our commitment to excellence ensures that you receive exceptional care and optimal outcomes throughout your brachioplasty journey. Trust Cosmed Travel to prioritize your well-being and deliver remarkable results, all while providing cost savings that make your experience even more rewarding."
+)
+
+liposuction = BodyTreatment.find_or_create_by(
+  body_treatment_name: "Liposuction",
+  body_treatment_description: "At Cosmed Travel, we offer a diverse range of surgical liposuction and liposculpture procedures at highly affordable prices. Enjoy significant cost savings without compromising the quality of your healthcare. Our unwavering dedication to excellence guarantees that you will receive exceptional care and achieve optimal outcomes throughout your liposuction journey. Trust us to prioritize your well-being, ensuring remarkable results while maintaining affordability that enhances your overall experience."
+)
+
+vaser_liposuction = BodyTreatment.find_or_create_by(
+  body_treatment_name: "Vaser Liposuction",
+  body_treatment_description: "If you are seeking vaser liposuction, you have arrived at the perfect destination. Our team of vaser liposuction specialists at Cosmed Travel has assisted numerous clients globally in improving their physique. Achieve the desired results you seek at a remarkable cost savings of 30% to 50% compared to prices in Europe. Trust Cosmed Travel to deliver the outcomes you desire while providing exceptional value for your investment."
+)
+
+puts "body treatments created"
